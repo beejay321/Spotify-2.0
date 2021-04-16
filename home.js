7761d1e4ef3c3292261ed95f4515e02fd47a600f
tr3yd4n <tr3yd4n@gmail.com>
2021-04-16 11:50:34 +0100

    commit-3

--- home.js
+++ home.js
@@ -45,38 +45,47 @@ function switchContent(classOnOff) {
     }
 }
 
-// window.onload = function () {
-//     ()
-// }
+window.onload = function () {
+    getArtistCards()
+}
 
 
 
 console.log("HELLO")
 const getArtistCards = () => {
     console.log("hello")
+
     fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem")
         .then(resolution => resolution.json())
         .then(resolution => {
+
             console.log("HI")
             console.log(resolution)
-            for (const artist of resolution) createTheCard(artist)
-
+            console.log(resolution.data)
+            eminem = resolution.data
+            eminem.forEach(eminem => {
+                createTheCard(eminem)
+            })
         })
+
 }
 
-const createTheCard = (artist) => {
+const container = document.getElementById("row")
+
+const createTheCard = (eminem) => {
 
     const artistTemplate =
         `
-            <div class="card" >
-                <img src="${artist.picture}" class="card-img-top" alt="...">
-                <div class="card-body">
-                <h5 class="card-title">${artist.title}</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
+           <div class="card col-6 col-md-4 col-lg-2">
+            <div class="album-container card">
+                <img src="${eminem.album.cover_medium}" class="img-fluid">
+                    <img id="logo" src="spotifylogo.png" width="30" height="30" alt="">
+                        <p class="album-artist card-text">${eminem.title}</p>
             </div>
+        </div>
         `
+
+
     container.innerHTML += artistTemplate
 }
 
