444f73b58e2319f63916156b39dba6e1c8e94d12
tr3yd4n <tr3yd4n@gmail.com>
2021-04-16 09:38:33 +0100

    commit1

--- home.js
+++ home.js
@@ -44,3 +44,33 @@ function switchContent(classOnOff) {
             console.error("Error")
     }
 }
+
+// window.onload = function () {
+//     ()
+// }
+
+console.log("HELLO")
+const getArtistCards = () => {
+    console.log("hello")
+    fetch("http://striveschool-api.herokuapp.com/api/deezer/artist/412")
+        .then(resolution => resolution.json())
+        .then(resolution => {
+            console.log("HI")
+            console.log(resolution)
+
+        })
+}
+
+
+
+
+
+
+// template for card <div class="row no-gutters">
+// <div class="col-6 col-md-4 col-lg-2">
+//     <div class="album-container card">
+//         <img src="spice.jpg" class="img-fluid">
+//         <img id="logo" src="spotifylogo.png" width="30" height="30" alt="">
+//         <p class="album-artist card-text">The Spice Girls</p>
+//     </div>
+// </div>
